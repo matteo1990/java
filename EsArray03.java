eda90efc54af56edd4ebb5f808d32431eba88621
Matteo1990 <kandeliere@libero.it>
2016-02-19 10:15:47 +0100

    es3 added

--- EsArray03.java
+++ EsArray03.java
@@ -7,12 +7,13 @@ public class EsArray03{
 	
 			Scanner tastiera = new Scanner(System.in);
 			int[] arrayNum = {44,11,5,8,10,3};
+			
 			show(arrayNum);
 			max(arrayNum);
-			min(arrayNum);
-			
 		
 	}
+	
+	
 	
 	public static void show(int [] a) {
 
@@ -23,26 +24,38 @@ public class EsArray03{
 	
 		}
 		
-	public static void max(int [] a) {
+		
+		
+	public static int[] max(int [] a) {
 			int max = 0;
-			
-		for (int i=0; i<a.length; i++){
-			if (a[i]>max){
-				max = a[i];
-				}
-			}
-		System.out.println("Il maggiore è: " + max);
-		}
-		
-	public static void min(int [] a) {
+			int posmax = 0;
 			int min = a[0];
+			int posmin = 0;
 			
 		for (int i=0; i<a.length; i++){
-			if (a[i]<min){
+			if (a[i]>max) {
+			max=a[i];
+			posmax = i;}
+			else if 
+			(a[i]<min){
 				min = a[i];
-				}
-			}
+				posmin = i;
+			}	
+		}	
+		System.out.println("Il maggiore è: " + max);
+		System.out.println("La posizione del maggiore è: " + posmax);
 		System.out.println("Il minore è: " + min);
-		}
+		System.out.println("La posizione del minimo è" + posmin);
 		
+		a[posmax]=min;
+		a[posmin]=max;
+		
+		
+		for (int i=0; i<a.length; i++) {
+			System.out.println(a[i]);
+			}
+			
+		return a;
+		}
 }
+
